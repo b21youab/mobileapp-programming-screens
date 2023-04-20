5069dcf5a2f59ada5a6d059c7179deaf11ef0197
Youssuf <b21youab@his.student.se>
2023-04-20 10:29:45 +0200

    Instantiated intent and have now have a button to click that redicrects to second activity.

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        Button sendButton = findViewById(R.id. sendButton);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, ViewActivity.class);
+                intent.putExtra("name", "Daniel"); // Optional
+                intent.putExtra("number", 1); // Optional
+                startActivity(intent);
+            }
+
+
+    });
+}
 }
